d84f3217dd9eee68323d3a7ebb450e4e95eb28a0
mica <felder.micaela2009@gmail.com>
2021-11-15 19:21:23 -0300

    post user - red

---
+++ AdministrationServerWebApi/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdministrationServerWebApi.GrpcClient;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AdministrationServerWebApi
+{
+    public class Program
+    {
+        public static Greeter.GreeterClient Client { get; set; }
+        
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            Client = new Greeter.GreeterClient(channel);
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(5001, o => o.Protocols = 
+                            HttpProtocols.Http2);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
